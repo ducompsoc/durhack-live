8afd53fdc3f5fc89243832341c29572864e2e86b
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-13 01:03:16 +0100

    feat: website builds on NextJS

--- oldclient/src/components/common/ContentContainer.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const ContentContainer = styled.div`
-	width: 1200px;
-	margin: 0px auto;
-	padding: 0px;
-`;
--- oldclient/src/components/common/Section.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Section = styled.div`
-	padding: 0px 0px;
-	margin: 0px -9px 18px -9px;
-`;
