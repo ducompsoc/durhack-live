a3a7969c834562197c85a1c2cb159d6e73c13537
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-13 23:46:29 +0100

    use 'declare' instead of 'private' to fix sequelize

--- server/src/database.ts
+++ server/src/database.ts
@@ -2,36 +2,36 @@ import { DataTypes, Model, Sequelize } from 'sequelize';
 import config from 'config';
 
 export const sequelize = new Sequelize({
-    host: config.get('mysql.host'),
-    database: config.get('mysql.name'),
-    username: config.get('mysql.user'),
-    password: config.get('mysql.pass'),
-    dialect: 'mysql',
+  host: config.get('mysql.host'),
+  database: config.get('mysql.name'),
+  username: config.get('mysql.user'),
+  password: config.get('mysql.pass'),
+  dialect: 'mysql',
 });
 
 export class User extends Model {
-    public id!: number;
-    public email!: string;
-    public hashed_password!: Buffer | null;
-    public password_salt!: Buffer | null;
-    public preferredName!: string;
-    public fullName!: string;
-    public role!: string | null;
-    public verifyCode!: string | null;
-    public verifySentAt!: Date | null;
-    public initiallyLoggedInAt!: Date | null;
-    public lastLoggedInAt!: Date | null;
-    public age!: number | null;
-    public phoneNumber!: string | null;
-    public university!: string | null;
-    public graduationYear!: string | null;
-    public ethnicity!: string | null;
-    public gender!: string | null;
-    public hUKMarketing!: boolean | null;
-    public hUKConsent!: boolean | null;
-    public checkedIn!: boolean | null;
-    public discordId!: string | null;
-    public discordName!: string | null;
+  declare id!: number;
+  declare email!: string;
+  declare hashed_password!: Buffer | null;
+  declare password_salt!: Buffer | null;
+  declare preferredName!: string;
+  declare fullName!: string;
+  declare role!: string | null;
+  declare verifyCode!: string | null;
+  declare verifySentAt!: Date | null;
+  declare initiallyLoggedInAt!: Date | null;
+  declare lastLoggedInAt!: Date | null;
+  declare age!: number | null;
+  declare phoneNumber!: string | null;
+  declare university!: string | null;
+  declare graduationYear!: string | null;
+  declare ethnicity!: string | null;
+  declare gender!: string | null;
+  declare hUKMarketing!: boolean | null;
+  declare hUKConsent!: boolean | null;
+  declare checkedIn!: boolean | null;
+  declare discordId!: string | null;
+  declare discordName!: string | null;
 }
 
 User.init(
