b41b98f8803831d65aff2fb23442e8c140253e44
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-15 03:17:58 +0100

    add csrf mitigating config

---
+++ server/src/auth/csrf.ts
@@ -0,0 +1,15 @@
+import config from "config";
+import { Request, Response } from "express";
+import { doubleCsrf } from "csrf-csrf";
+
+
+const options = config.get("csrf.options") as any;
+options.getSecret = () => config.get("csrf.secret");
+
+export const { generateToken, doubleCsrfProtection } = doubleCsrf(options);
+
+export function handleGetCsrfToken(request: Request, response: Response): void {
+  const csrfToken = generateToken(response, request);
+  response.status(200);
+  response.json({ "status": 200, "message": "Token generation OK", "token": csrfToken });
+}
