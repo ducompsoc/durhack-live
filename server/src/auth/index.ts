8abafdbf23404b2aee2a48ea2f6b003abccdaa98
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-15 03:18:51 +0100

    add auth index - import strategies and implement serialize/deserialize user logic

---
+++ server/src/auth/index.ts
@@ -0,0 +1,33 @@
+import passport from "passport";
+
+import SequelizeUser from "@/database/user";
+import { NullError } from "@/common/errors";
+
+import "./strategy/local";
+import "./strategy/bearer";
+
+declare global {
+  namespace Express {
+    interface User extends SequelizeUser {}
+  }
+}
+
+interface SerializedUser {
+  id: number;
+}
+
+passport.serializeUser<SerializedUser>(async function(user: Express.User, callback) {
+  return callback(null, { id: user.id });
+});
+
+passport.deserializeUser<SerializedUser>(async function(identifier, callback) {
+  if (typeof identifier?.id !== "number") {
+    return callback(null, null);
+  }
+  try {
+    return callback(null, await SequelizeUser.findByPk(identifier.id, { rejectOnEmpty: new NullError() }));
+  } catch (error) {
+    if (error instanceof NullError) return callback(null, null);
+    return callback(error);
+  }
+});
