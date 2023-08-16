6f64cc8c98f3bdd7cf68f8ed6e412317d6a54cd6
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-16 04:40:01 +0100

    use session options schema to parse session options

--- server/src/auth/session.ts
+++ server/src/auth/session.ts
@@ -3,6 +3,8 @@ import session, { MemoryStore, Store, SessionOptions } from "express-session";
 import * as constructor_session from "express-session";
 import MySQLStoreMeta, { MySQLStore as MySQLStoreType, Options as MySqlStoreOptions } from "express-mysql-session";
 
+import { session_options_schema } from "@/common/config_schema";
+
 function get_mysql_session_store(): MySQLStoreType {
   const MySQLStore = MySQLStoreMeta(constructor_session);
   const options = config.get("mysql.session") as MySqlStoreOptions;
@@ -23,7 +25,7 @@ function get_session_store(): Store {
 
 const sessionStore = get_session_store();
 
-const session_options = config.get("session") as SessionOptions;
+const session_options = session_options_schema.parse(config.get("session")) as SessionOptions;
 session_options.store = sessionStore;
 
 export default session(session_options);
