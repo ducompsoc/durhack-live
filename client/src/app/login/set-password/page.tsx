3b1413208dcef8d0d0611eb66bacf7e402bd2b98
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-18 02:17:24 +0100

    complete set-password page

--- client/src/app/login/set-password/page.tsx
+++ client/src/app/login/set-password/page.tsx
@@ -4,30 +4,116 @@ import React from 'react';
 import { Button, ErrorAlert, FormSection, Textbox } from '@/app/login/components';
 import { Formik, ErrorMessage, Form } from 'formik';
 import { useRouter } from 'next/navigation';
+import { makeLiveApiRequest } from '@/app/util/api';
 
 export default function SetPasswordPage() {
+  const [email, setEmail] = React.useState<string>();
   const [error, setError] = React.useState<string>();
   const [verifyCode, setVerifyCode] = React.useState<string>();
   const router = useRouter();
 
-  function handleVerifyCodeSubmit(verify_code: string): void {
+  React.useEffect( () => {
+    (async () => {
+      const query_email = new URLSearchParams(window.location.search).get('email');
+      if (!query_email) return router.push('/login');
+      setEmail(query_email);
 
+      await requestVerifyCode();
+    })();
+  });
+
+  function setUnknownError() {
+    setError(`An unknown error occurred. Please try a refresh, and if you\'re still having problems,
+      ask a member of the DurHack team.`
+    );
   }
 
-  function handleSetPasswordSubmit(password: string): void {
+  async function requestVerifyCode(send_again = false) {
+    const send_verify_request = await makeLiveApiRequest('/auth/verify-email', {
+      method: 'POST',
+      body: new URLSearchParams({ email: email, send_again: send_again }),
+    });
 
+    let send_verify_response: Response;
+    try {
+      send_verify_response = await fetch(send_verify_request);
+    } catch (error) {
+      return setUnknownError();
+    }
+
+    if (send_verify_response.status === 304) return;
+
+    if (send_verify_response.status === 404) return router.push('/login');
+
+    if (!send_verify_response.ok) return setUnknownError();
   }
 
-  const handleSubmit = React.useCallback((submission: { verify_code?: string, password?: string }) => {
+  async function handleVerifyCodeSubmit(submission_verify_code: string): Promise<void> {
+    const check_verify_request = await makeLiveApiRequest('/auth/check-verify', {
+      method: 'POST',
+      body: new URLSearchParams({ email: email, verify_code: submission_verify_code }),
+    });
+
+    let check_verify_response: Response;
+    try {
+      check_verify_response = await fetch(check_verify_request);
+    } catch (error) {
+      return setUnknownError();
+    }
+
+    if (check_verify_response.status === 400) return setError('That\'s not the right code, or it\'s expired - try again.');
+    if (check_verify_response.status === 404) return router.push('/login');
+    if (!check_verify_response.ok) return setUnknownError();
+
+    setVerifyCode(submission_verify_code);
+  }
+
+  async function handleSetPasswordSubmit(submission_password: string): Promise<void> {
+    const set_password_request = await makeLiveApiRequest('/auth/set-password', {
+      method: 'POST',
+      body: new URLSearchParams({ email: email, verify_code: verifyCode, password: submission_password }),
+    });
+
+    let set_password_response: Response;
+    try {
+      set_password_response = await fetch(set_password_request);
+    } catch (error) {
+      return setUnknownError();
+    }
+
+    if (set_password_response.status === 400) return setError('That\'s not the right code, or it\'s expired - try again.');
+    if (set_password_response.status === 404) return router.push('/login');
+    if (!set_password_response.ok) return setUnknownError();
+
+    const profile_request = await makeLiveApiRequest('/users/me');
+    let profile_response: Response;
+    try {
+      profile_response = await fetch(profile_request);
+    } catch (error) {
+      return setUnknownError();
+    }
+
+    if (!profile_response.ok) {
+      return setUnknownError();
+    }
+
+    const profile = await profile_response.json();
+    if (!profile.checked_in) {
+      return router.push('/login/check-in');
+    }
+    return router.push('/');
+  }
+
+  const handleSubmit = React.useCallback(async (submission: { verify_code?: string, password?: string }) => {
     setError(undefined);
     console.log(submission);
 
     if (submission.password) {
-      return handleSetPasswordSubmit(submission.password);
+      return await handleSetPasswordSubmit(submission.password);
     }
 
     if (submission.verify_code) {
-      return handleVerifyCodeSubmit(submission.verify_code);
+      return await handleVerifyCodeSubmit(submission.verify_code);
     }
 
     throw new Error('Validation failed.');
