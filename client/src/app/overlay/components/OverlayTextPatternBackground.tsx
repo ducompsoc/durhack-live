98e23fd15118edde6eb3ba03cb8616d8a488b0e1
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-25 02:08:48 +0100

    implement overlay main wrapper and milestone

---
+++ client/src/app/overlay/components/OverlayTextPatternBackground.tsx
@@ -0,0 +1,35 @@
+export default function TextPatternBackground() {
+  return (
+    <div className="text-pattern-bg">
+      <div className="lines">
+        <div className="a"></div>
+        <div className="b"></div>
+        <div className="c"></div>
+        <div className="d"></div>
+        <div className="e"></div>
+        <div className="a"></div>
+        <div className="b"></div>
+        <div className="c"></div>
+        <div className="d"></div>
+        <div className="e"></div>
+        <div className="a"></div>
+        <div className="b"></div>
+        <div className="c"></div>
+        <div className="d"></div>
+        <div className="e"></div>
+        <div className="a"></div>
+        <div className="b"></div>
+        <div className="c"></div>
+        <div className="d"></div>
+        <div className="e"></div>
+      </div>
+
+      <div className="foreground">
+        <div>
+          <div className="countdown"></div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
