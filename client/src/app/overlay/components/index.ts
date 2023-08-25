5e1c7a9d7d80eda271208a9caca97fe831665f50
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-25 02:09:24 +0100

    re-export overlay components

--- client/src/app/overlay/components/index.ts
+++ client/src/app/overlay/components/index.ts
@@ -4,3 +4,4 @@ export { default as OverlayMainWrapper } from "./OverlayMainWrapper";
 export { default as OverlayMilestone } from "./OverlayMilestone";
 export { default as OverlaySlides } from "./OverlaySlides";
 export { default as OverlayUpperThird } from "./OverlayUpperThird";
+export { default as OverlayTextPatternBackground } from "./OverlayTextPatternBackground";
