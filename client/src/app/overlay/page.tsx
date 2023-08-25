c435942a983ed915640889133214fd24b6395d5f
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-25 02:09:13 +0100

    remove uneeded imports; move text-pattern-bg to its own component

--- client/src/app/overlay/page.tsx
+++ client/src/app/overlay/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { io, Socket } from "socket.io-client";
-import OBSWebSocket from "obs-websocket-js";
-import { isEqual } from "lodash";
 import YouTube, { YouTubeProps } from "react-youtube";
 
 import {
@@ -29,41 +26,9 @@ export default function LivestreamOverlay() {
         <OverlayLowerThird />
 
         <OverlayMainWrapper>
-          <OverlaySlides/>
-          <OverlayMilestone/>
+          <OverlaySlides />
+          <OverlayMilestone />
         </OverlayMainWrapper>
-
-        <div className="text-pattern-bg">
-          <div className="lines">
-            <div className="a"></div>
-            <div className="b"></div>
-            <div className="c"></div>
-            <div className="d"></div>
-            <div className="e"></div>
-            <div className="a"></div>
-            <div className="b"></div>
-            <div className="c"></div>
-            <div className="d"></div>
-            <div className="e"></div>
-            <div className="a"></div>
-            <div className="b"></div>
-            <div className="c"></div>
-            <div className="d"></div>
-            <div className="e"></div>
-            <div className="a"></div>
-            <div className="b"></div>
-            <div className="c"></div>
-            <div className="d"></div>
-            <div className="e"></div>
-          </div>
-
-          <div className="foreground">
-            <div>
-              <div className="countdown"></div>
-            </div>
-          </div>
-
-        </div>
       </div>
     </main>
   );
