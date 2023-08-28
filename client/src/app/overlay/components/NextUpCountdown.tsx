f6c7adfde64ff168a5afa23e3333d629c267bae9
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-28 02:29:21 +0100

    create NextUpCountdown component

---
+++ client/src/app/overlay/components/NextUpCountdown.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+
+import React from "react";
+
+import useCountdown from "@/app/util/countdownHook";
+import { zeroPad } from "@/app/util/util";
+
+function digitise(num: string) {
+  return num.split("").map((char, index) => <span className="digit" key={index}>{char}</span>);
+}
+
+declare type NextUpCountdownProps = {
+  countdownTo: string | Date;
+  className?: string;
+  separatorClassName?: string;
+}
+
+export default function NextUpCountdown(props: NextUpCountdownProps) {
+  let { countdownTo } = props;
+
+  if (typeof countdownTo === "string") {
+    countdownTo = new Date(countdownTo);
+  }
+
+  const countdownValues = useCountdown(countdownTo, 500);
+
+  if (countdownTo.toString() === "Invalid Date") return <></>;
+
+  return (
+    <div className={props.className} >
+      {digitise(countdownValues.minutes.toString())}
+      <span className={props.separatorClassName}>:</span>
+      {digitise(zeroPad(countdownValues.seconds))}
+    </div>
+  );
+}
