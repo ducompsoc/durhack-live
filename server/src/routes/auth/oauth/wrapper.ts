592196d4466be34738b49fb73a836b1744f703e8
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-09-06 19:27:41 +0100

    :bug: fix oauth skipping all middleware

--- server/src/routes/auth/oauth/wrapper.ts
+++ server/src/routes/auth/oauth/wrapper.ts
@@ -2,26 +2,38 @@ import ExpressOAuthServer from "@node-oauth/express-oauth-server";
 
 import Model, { oauth_config } from "@/routes/auth/oauth/model";
 import { NextFunction, Request, Response } from "express";
-import OAuth2Server from "@node-oauth/oauth2-server";
+import OAuth2Server, {UnauthorizedRequestError} from "@node-oauth/oauth2-server";
 
 
-class UserLoadingExpressOAuthServer extends ExpressOAuthServer {
-  authenticate(options?: OAuth2Server.AuthenticateOptions): (request: Request, response: Response, next: NextFunction) => Promise<OAuth2Server.Token> {
+declare class ExpressOAuthServer {
+  _handleError(response: Response, oauthResponse: OAuth2Server.Response, error: Error, next: NextFunction): void;
+}
+
+class DurhackExpressOAuthServer extends ExpressOAuthServer {
+  override authenticate(options?: OAuth2Server.AuthenticateOptions): (request: Request, response: Response, next: NextFunction) => Promise<OAuth2Server.Token> {
     const middleware = super.authenticate(options);
 
     return async function(request: Request, response: Response, next: NextFunction): Promise<OAuth2Server.Token> {
       const result = await middleware(request, response, next);
 
-      if (typeof response.locals.oauth.token === "object") {
+      if (typeof response.locals.oauth?.token === "object") {
         request.user = response.locals.oauth.token.user;
       }
 
       return result;
     };
   }
+
+  override _handleError(response: Response, oauthResponse: OAuth2Server.Response, error: Error, next: NextFunction) {
+    if (error instanceof UnauthorizedRequestError) {
+      response.set(oauthResponse.headers);
+      return next();
+    }
+    super._handleError(response, oauthResponse, error, next);
+  }
 }
 
-const wrapped_oauth_provider = new UserLoadingExpressOAuthServer(
+const wrapped_oauth_provider = new DurhackExpressOAuthServer(
   {
     model: Model,
     accessTokenLifetime: oauth_config.accessTokenLifetime,
