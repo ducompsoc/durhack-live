04fee89c84c535f5b704a005510ec78c2f347a3f
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-09-10 18:19:27 +0100

    add OAuth route handling

--- server/src/routes/auth/oauth/index.ts
+++ server/src/routes/auth/oauth/index.ts
@@ -1,51 +1,18 @@
-import { Request, Response, Router as ExpressRouter} from "express";
-import { z } from "zod";
+import { Router as ExpressRouter} from "express";
 
 import { handleMethodNotAllowed } from "@/common/middleware";
-import wrapped_oauth_provider from "@/routes/auth/oauth/wrapper";
-import { OAuthClient } from "@/database/tables";
-import {NullError} from "@/common/errors";
-
-
-const authorize_get_query_params = z.object({
-  client_id: z.string(),
-});
+import handlers from "@/routes/auth/oauth/oauth_handlers";
 
 
 const oauth_router = ExpressRouter();
 
 oauth_router.route("/authorize")
-  .get(async (request: Request, response: Response) => {
-    if (!request.user) {
-      request.session.redirect_to = "/login/authorize" + new URL(request.originalUrl).search;
-      response.redirect("/login");
-      return;
-    }
-
-    const { client_id } = authorize_get_query_params.parse(request.query);
-    const client = await OAuthClient.findByPk(client_id, { rejectOnEmpty: new NullError("OAuth client not found.") });
-
-    response.status(200);
-    response.json({
-      status: response.statusCode,
-      message: "OK",
-      user: {
-        name: request.user.preferred_name,
-      },
-      client:{
-        name: client.name,
-      }
-    });
-  })
-  .post(wrapped_oauth_provider.authorize({
-    authenticateHandler: {
-      handle: (request: Request) => request.user,
-    }
-  }))
+  .get(handlers.getAuthorize)
+  .post(handlers.postAuthorize)
   .all(handleMethodNotAllowed);
 
 oauth_router.route("/token")
-  .post(wrapped_oauth_provider.token())
+  .post(handlers.postToken)
   .all(handleMethodNotAllowed);
 
 
