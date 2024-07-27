ecdd06f8560352991a03c0d05fdddeab15a447c9
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-07-27 17:29:37 +0100

    fix: swap model field names and `@map`ped names

--- server/prisma/migrations/0_init/migration.sql
+++ server/prisma/migrations/0_init/migration.sql
@@ -1,40 +1,40 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "keycloakUserId" UUID NOT NULL,
-    "keycloakAccessToken" TEXT,
-    "keycloakRefreshToken" TEXT,
+    "keycloak_user_id" UUID NOT NULL,
+    "keycloak_access_token" TEXT,
+    "keycloak_refresh_token" TEXT,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("keycloakUserId")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("keycloak_user_id")
 );
 
 -- CreateTable
 CREATE TABLE "OAuthClient" (
-    "clientId" VARCHAR(256) NOT NULL,
+    "client_id" VARCHAR(256) NOT NULL,
     "name" VARCHAR(256) NOT NULL,
     "grants" JSONB NOT NULL DEFAULT '[]',
-    "allowedScopes" JSONB NOT NULL DEFAULT '[]',
-    "redirectUris" JSONB NOT NULL DEFAULT '[]',
-    "accessTokenLifetime" INTEGER,
-    "refreshTokenLifetime" INTEGER,
-    "hashedSecret" BYTEA,
-    "secretSalt" BYTEA,
+    "allowed_scopes" JSONB NOT NULL DEFAULT '[]',
+    "redirect_uris" JSONB NOT NULL DEFAULT '[]',
+    "access_token_lifetime" INTEGER,
+    "refresh_token_lifetime" INTEGER,
+    "hashed_secret" BYTEA,
+    "secret_salt" BYTEA,
 
-    CONSTRAINT "OAuthClient_pkey" PRIMARY KEY ("clientId")
+    CONSTRAINT "OAuthClient_pkey" PRIMARY KEY ("client_id")
 );
 
 -- CreateTable
 CREATE TABLE "OAuthUser" (
-    "userId" UUID NOT NULL,
-    "clientId" VARCHAR(256) NOT NULL,
-    "minimumTokenIssueTime" TIMESTAMP(3),
-    "minimumAuthCodeIssueTime" TIMESTAMP(3),
+    "user_id" UUID NOT NULL,
+    "client_id" VARCHAR(256) NOT NULL,
+    "minimum_token_issue_time" TIMESTAMP(3),
+    "minimum_auth_code_issue_time" TIMESTAMP(3),
 
-    CONSTRAINT "OAuthUser_pkey" PRIMARY KEY ("userId","clientId")
+    CONSTRAINT "OAuthUser_pkey" PRIMARY KEY ("user_id","client_id")
 );
 
 -- AddForeignKey
-ALTER TABLE "OAuthUser" ADD CONSTRAINT "OAuthUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("keycloakUserId") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "OAuthUser" ADD CONSTRAINT "OAuthUser_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("keycloak_user_id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "OAuthUser" ADD CONSTRAINT "OAuthUser_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "OAuthClient"("clientId") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "OAuthUser" ADD CONSTRAINT "OAuthUser_client_id_fkey" FOREIGN KEY ("client_id") REFERENCES "OAuthClient"("client_id") ON DELETE CASCADE ON UPDATE CASCADE;
 
