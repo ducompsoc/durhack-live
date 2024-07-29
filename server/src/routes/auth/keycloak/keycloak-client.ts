5884eae6c05eaee73b701dab2d0d896e77f91664
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-07-29 18:48:55 +0100

    feat: implement utility method for fetching with keycloak client credentials

--- server/src/routes/auth/keycloak/keycloak-client.ts
+++ server/src/routes/auth/keycloak/keycloak-client.ts
@@ -15,3 +15,29 @@ export const keycloakIssuer = await Issuer.discover(keycloakConfig.url)
 
 const keycloakClientConfig = adaptClientConfig(keycloakConfig)
 export const keycloakClient = new keycloakIssuer.Client(keycloakClientConfig)
+
+async function fetchKeycloakClientCredentials() {
+  return await keycloakClient.grant({
+    grant_type: "client_credentials",
+  })
+}
+
+let keycloakClientCredentials = await fetchKeycloakClientCredentials()
+
+export async function getKeycloakClientCredentials() {
+  if (!keycloakClientCredentials.expired()) return keycloakClientCredentials
+  keycloakClientCredentials = await fetchKeycloakClientCredentials()
+  return keycloakClientCredentials
+}
+
+export async function fetchWithClientCredentials(input: string | URL | Request, init?: RequestInit): Promise<Response> {
+  const { headers, ...restInit } = init ?? {}
+  const credentials = await getKeycloakClientCredentials()
+  return await fetch(input, { 
+    headers: {
+      Authorization: `Bearer ${keycloakClientCredentials.access_token}`,
+      ...headers,
+    },
+    ...restInit,
+  })
+}
