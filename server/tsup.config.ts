9d58f9b80037c2bf46100257500f7349e29dd29b
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-27 13:48:14 +0000

    fix: remove entry-point that does not exist

--- server/tsup.config.ts
+++ server/tsup.config.ts
@@ -4,7 +4,6 @@ export default defineConfig([
   {
     entry: {
       main: "src/main.ts",
-      "ticket-assigner": "src/ticket-assigner/main.ts",
     },
     target: "node22",
     format: "esm",
