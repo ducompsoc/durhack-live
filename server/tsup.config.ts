287961c42c19e5d5a6dbec1019c064f7fb7c003f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-10-27 13:46:59 +0000

    use tsup configuration from `durhack`

--- server/tsup.config.ts
+++ server/tsup.config.ts
@@ -1,26 +1,17 @@
-import * as path from "node:path"
-import * as glob from "glob"
 import { defineConfig } from "tsup"
-
-const configFilePaths = glob.sync("src/config/*.ts")
-const configFileEntryArray = configFilePaths
-  .filter((filePath) => !filePath.endsWith("index.ts") && !filePath.endsWith("schema.ts"))
-  .map((filePath) => {
-    return [`config/${path.parse(filePath).name}`, filePath]
-  })
-const configFileEntries = Object.fromEntries(configFileEntryArray)
 
 export default defineConfig([
   {
     entry: {
       main: "src/main.ts",
-      ...configFileEntries,
+      "ticket-assigner": "src/ticket-assigner/main.ts",
     },
-    target: "esnext",
+    target: "node22",
     format: "esm",
     splitting: true,
     clean: true,
     minify: false,
     outDir: "dist",
+    external: ["tsx"],
   },
 ])
